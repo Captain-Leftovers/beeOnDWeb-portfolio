9e432d2cb09ae8b044d946b72eb48ecd6b5a3fbe
Dob <lalilulelo.mg@gmail.com>
2023-06-14 18:12:13 +1000

    fixed linting error not escaped apost

--- src/app/about/components/Introduction/AboutMe.tsx
+++ src/app/about/components/Introduction/AboutMe.tsx
@@ -3,27 +3,27 @@ export default function AboutMe() {
 		<div className="col-span-4">
 			<h1 className="text-4xl font-medium">About Me</h1>
 			<p className="mt-4 text-lg">
-				Hello! I'm Dob, a self-taught Full Stack
+				Hello! I&apos;m Dob, a self-taught Full Stack
 				Developer with a passion for coding and
-				technology. Over the last three years, I've
+				technology. Over the last three years, I&apos;ve
 				immersed myself in learning the ins and outs of
 				web development. From mastering JavaScript to
 				exploring the vast capabilities of libraries
 				like React, and diving into server-side
-				rendering with Next.js, I've enjoyed every bit
+				rendering with Next.js, I&apos;ve enjoyed every bit
 				of the journey.
 			</p>
 			<p className="mt-4 text-lg">
 				As someone who thrives on challenge and
 				problem-solving, coding is more than just a
-				profession - it's a hobby and a way of life for
+				profession - it&apos;s a hobby and a way of life for
 				me. My focus is on writing clean, efficient code
 				and building interactive, responsive web
 				applications that provide a seamless user
 				experience.
 			</p>
 			<p className="mt-4 text-lg">
-				Although I'm relatively new to the industry, I'm
+				Although I&apos;m relatively new to the industry, I&apos;m
 				highly motivated, eager to learn, and excited to
 				bring my passion for web development to a
 				professional setting.
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
 
 		<article className="flex flex-col">
 			<h1 className="self-center text-4xl font-medium ">Hi there!</h1>
-			<p className="text-center ">I'm <span className="text-2xl font-medium text-primary">Dob</span>,<br/> Full Stack Programmer and Web Developer,<br/> fusing cutting-edge technology with your visions.</p>
+			<p className="text-center ">I&apos;m <span className="text-2xl font-medium text-primary">Dob</span>,<br/> Full Stack Programmer and Web Developer,<br/> fusing cutting-edge technology with your visions.</p>
 			<div className="flex items-center justify-center w-full mt-4">
 				<a download href='/resume.pdf'  className=" w-40 flex items-center justify-center p-2.5 px-6 border-2 border-solid rounded-md border-primary bg-primary font-semibold hover:bg-secondary">My Resume</a>
 				<a className="flex items-center justify-center ml-4 p-2.5 px-6 border-2 border-solid rounded-md border-primary font-semibold bg-secondary w-40 ">Get in touch</a>
