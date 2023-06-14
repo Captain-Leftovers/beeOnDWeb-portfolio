2ec34df3600a927d27a29137ca071c943e8f7731
Dob <lalilulelo.mg@gmail.com>
2023-06-15 06:57:26 +1000

    create light / dark  hook

--- src/hooks/useThemeSwitcher.tsx
+++ src/hooks/useThemeSwitcher.tsx
@@ -1,1 +1,32 @@
-//TODO : create theme switcher hook
+import { useEffect, useState } from "react";
+
+
+
+export default function useThemeSwitcher() {
+
+    const preferDarkQuery = '(prefers-color-scheme: dark)';
+
+    const [mode, setMode] = useState<'light' | 'dark'>(
+        () => (localStorage.getItem('color-mode') as 'light' | 'dark') || 'light'
+    );
+
+    useEffect(() => {
+        const mediaQuery = window.matchMedia(preferDarkQuery);
+        const handleChange = () => {    
+            setMode(mediaQuery.matches ? 'dark' : 'light');
+        };
+        mediaQuery.addEventListener('change', handleChange);
+        return () => mediaQuery.removeEventListener('change', handleChange);
+    }, []);
+
+    useEffect(() => {
+        localStorage.setItem('color-mode', mode);
+    }, [mode]);
+
+
+
+
+  return (
+    <div>useThemeSwitcher</div>
+  )
+}
