5d8b271af3d89860b8288eba3fa734926b68dd43
Dobromir Stefanov Ivanov <lalilulelo.mg@gmail.com>
2023-06-18 06:50:54 +1000

    some styles to projects page

--- src/app/projects/components/BigProjCard.tsx
+++ src/app/projects/components/BigProjCard.tsx
@@ -3,11 +3,16 @@ import laptop from '#/laptopBeeLog.png'
 
 export default function BigProjCard() {
 	return (
-		<div className="w-1/2 bg-red-300 ">
-			<div className="bg-accent  w-full max-w-screen-lg mx-auto aspect-video flex relative ">
-				<div className=" w-full bg-yellow-300 ">
-					<h1>BeeKeeper's Log</h1>
-					<h2>React, Typescript, Prisma, TRPC</h2>
+		<div className="w-1/2  ">
+			<div className=" bg-accent/40  w-full max-w-screen-lg mx-auto aspect-video flex relative ">
+				<div className=" w-1/2  flex flex-col justify-center pl-8 gap-6">
+					<h2 className='text-2xl'>Build with : React, Typescript, Prisma, TRPC</h2>
+					<h1 className='text-4xl'>BeeKeeper's Log</h1>
+					<section>
+					<p>Track hive health</p>
+					<p>Store information related to hive inspection</p>
+
+					</section>
 				</div>
 				<div className='absolute -right-20 -bottom-20 w-2/3'>
 					<picture className="">
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -8,9 +8,15 @@ export const metadata = {
 
 export default function Projects() {
   return (
-    <main className="flex flex-col items-center justify-center grow w-full max-w-screen-2xl " >
-     //TODO : Add projects cards and some slogan
+    <main className="flex flex-col items-center justify-center grow w-full max-w-screen-2xl gap-40 " >
+      <div>
 
+    <h1 className="text-6xl font-medium max-w-screen-lg text-center mt-20 ">Exploring the intersection of technology and creativity.</h1>
+      </div>
+
+    <BigProjCard />
+    <BigProjCard />
+    <BigProjCard />
     <BigProjCard />
 
 
