3d3d930c28ab9a0cfdcf0072b67dc48a4a78c818
Dobromir Stefanov Ivanov <lalilulelo.mg@gmail.com>
2023-06-18 07:55:37 +1000

    added span to prep for cosmetic line in middle

--- src/app/projects/components/BigProjCard.tsx
+++ src/app/projects/components/BigProjCard.tsx
@@ -5,9 +5,12 @@ export default function BigProjCard() {
 	return (
 		<div className="w-1/2  ">
 			<div className=" bg-accent/40  w-full max-w-screen-lg mx-auto aspect-video flex relative ">
+				<span className='absolute'>
+					//TODO :  make this a line  for astetic purposes
+				</span>
 				<div className=" w-1/2  flex flex-col justify-center pl-8 gap-6">
 					<h2 className='text-2xl'>Build with : React, Typescript, Prisma, TRPC</h2>
-					<h1 className='text-4xl'>BeeKeeper's Log</h1>
+					<h1 className='text-4xl'>BeeKeeper&apos;s Log</h1>
 					<section>
 					<p>Track hive health</p>
 					<p>Store information related to hive inspection</p>
