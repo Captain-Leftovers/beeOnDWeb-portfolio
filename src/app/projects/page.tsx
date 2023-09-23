3fa94effa6845233957e186711a7c38591a31f83
Dob <lalilulelo.mg@gmail.com>
2023-09-24 07:41:55 +1000

    fixed comment so i can deploy

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -2,7 +2,6 @@ import BigProjCard from './components/BigProjCard'
 import beeLogLaptop from '#/laptopBeeLog.png'
 import mySpotLaptop from '#/laptopMySpot.png'
 
-
 export const metadata = {
 	title: 'Projects',
 	description: 'Projects page',
@@ -11,50 +10,52 @@ export const metadata = {
 export default function Projects() {
 	return (
 		<main className="w-full xl:max-w-screen-xl space-y-28 py-10">
-			<div className='w-full'>
+			<div className="w-full">
 				<h1 className="text-2xl mx-auto p-4 sm:text-6xl text-center font-medium xl:max-w-screen-lg ">
-					Exploring the intersection of technology
-					and creativity.
+					Exploring the intersection of technology and creativity.
 				</h1>
 			</div>
 
-			<BigProjCard 
-			buildWith='Build with : React, Typescript, Prisma, TRPC'  
-			name='BeeKeeper&apos;s Log'
-			description={['Track hive health','Store information about your hive inspections']}
-			imageSrc={mySpotLaptop}
-			cardColor='bg-blue-100'
-			githubLink='https://github.com/Captain-Leftovers/my-spot'
-			liveLink='https://my-spot.vercel.app/'
+			<BigProjCard
+				buildWith="Build with : React, Typescript, Prisma, TRPC"
+				name="BeeKeeper's Log"
+				description={[
+					'Track hive health',
+					'Store information about your hive inspections',
+				]}
+				imageSrc={mySpotLaptop}
+				cardColor="bg-blue-100"
+				githubLink="https://github.com/Captain-Leftovers/my-spot"
+				liveLink="https://my-spot.vercel.app/"
 			/>
-				<BigProjCard 
-			buildWith='Build with : React, Typescript, Prisma, TRPC'  
-			name='BeeKeeper&apos;s Log'
-			description={['Track hive health','Store information about your hive inspections']}
-			imageSrc={beeLogLaptop}
-			githubLink=''
-			cardColor='bg-green-100  dark:bg-amber-100'
-
-			
-
+			<BigProjCard
+				buildWith="Build with : React, Typescript, Prisma, TRPC"
+				name="BeeKeeper's Log"
+				description={[
+					'Track hive health',
+					'Store information about your hive inspections',
+				]}
+				imageSrc={beeLogLaptop}
+				githubLink=""
+				cardColor="bg-green-100  dark:bg-amber-100"
 			/>
 
-	//TODO : add all projects and link and maybe see the order
-
-			<BigProjCard 
-			buildWith='Build with : React, Typescript, Prisma, TRPC'  
-			name='BeeKeeper&apos;s Log'
-			description={['Track hive health','Store information about your hive inspections']}
-			imageSrc={beeLogLaptop}
-			cardColor='bg-amber-100  dark:bg-amber-200'
-			
-			githubLink='https://github.com/Captain-Leftovers/Captain-Leftovers-bee-log-react-trpc-prisma-postgresql-'
-			liveLink='https://beekeeperslog.com'
+			<BigProjCard
+				buildWith="Build with : React, Typescript, Prisma, TRPC"
+				name="BeeKeeper's Log"
+				description={[
+					'Track hive health',
+					'Store information about your hive inspections',
+				]}
+				imageSrc={beeLogLaptop}
+				cardColor="bg-amber-100  dark:bg-amber-200"
+				githubLink="https://github.com/Captain-Leftovers/Captain-Leftovers-bee-log-react-trpc-prisma-postgresql-"
+				liveLink="https://beekeeperslog.com"
 			/>
-			
 		</main>
 	)
 }
 
+//TODO : add all projects and link and maybe see the order
 
 //TODO : add all projects and hotos
