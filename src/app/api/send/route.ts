1912f75ca228a0e91e86b0aa597c5fca95e248d3
Dob <lalilulelo.mg@gmail.com>
2023-10-04 17:05:46 +1100

    fixed email to correct prod env

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -2,31 +2,28 @@ import { EmailTemplate } from '../../../components/EmailTemplate'
 import { NextResponse } from 'next/server'
 import { Resend } from 'resend'
 
-const resend = new Resend(process.env.RESEND_TEST)
-console.log('resend', resend)
+const resend = new Resend(process.env.RESEND_API_KEY)
 
 export async function POST(request: Request) {
 	const body = await request.json()
 	const { name, email, message } = body
 
-  let data;
-  try{
+	let data
+	try {
+		data = await resend.emails.send({
+			from: 'Contact Form <onboarding@resend.dev>',
+			to: ['beeondweb@gmail.com'],
+			subject: 'Portfolio Contact Form Message',
+			text: message,
+			react: EmailTemplate({ name, senderEmail: email, message }),
+		})
+	} catch (e: unknown) {
+		console.log('e', e)
 
-    data = await resend.emails.send({
-      from: 'Contact Form <onboarding@resend.dev>',
-      to: ['lalilulelo.mg@gmail.com'],
-      subject: 'Portfolio Contact Form Message',
-      text: message,
-      react: EmailTemplate({ name, senderEmail: email, message}),
-    })
-  }
-  catch(e: unknown){
-    console.log('e', e);
-    
-    return NextResponse.json({ error: e });
-  }
+		return NextResponse.json({ error: e })
+	}
 
-  console.log('data', data);
+	console.log('data', data)
 
-  return NextResponse.json(data);
+	return NextResponse.json(data)
 }
