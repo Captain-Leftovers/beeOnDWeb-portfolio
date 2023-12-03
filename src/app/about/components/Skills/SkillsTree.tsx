cddb44f2e0cbc303119b6ab9b63e8c22b3b82980
Dobromir Stefanov Ivanov <lalilulelo.mg@gmail.com>
2023-12-04 07:34:24 +1100

    added stagger animation to skill cards

--- src/app/about/components/Skills/SkillsTree.tsx
+++ src/app/about/components/Skills/SkillsTree.tsx
@@ -1,36 +1,89 @@
-import SkillCard from "./SkillCard"
-import NextJsSVG from "./NextJsSVG";
-import CssSVG from "./CssSVG"
-import JavaScriptSVG from "./JavaScriptSVG"
-import HtmlSVG from "./HtmlSVG"
-import ReactSVG from "./ReactSVG"
-import TypeScriptSVG from "./TypeScriptSVG";
-import TrpcSVG from "./TrpcSVG";
-import SqlSVG from "./SqlSVG";
-import PrismaSVG from "./PrismaSVG";
-import TailwindSVG from "./TailwindSVG";
-import GitSVG from "./GitSVG";
-import NodJsSVG from "./NodeJsSVG";
+'use client'
 
+import SkillCard from './SkillCard'
+import NextJsSVG from './NextJsSVG'
+import CssSVG from './CssSVG'
+import JavaScriptSVG from './JavaScriptSVG'
+import HtmlSVG from './HtmlSVG'
+import ReactSVG from './ReactSVG'
+import TypeScriptSVG from './TypeScriptSVG'
+import TrpcSVG from './TrpcSVG'
+import SqlSVG from './SqlSVG'
+import PrismaSVG from './PrismaSVG'
+import TailwindSVG from './TailwindSVG'
+import GitSVG from './GitSVG'
+import NodJsSVG from './NodeJsSVG'
+import { motion } from 'framer-motion'
 
-export default function SkillsTree() {
-	console.log();
+const container = {
+	hidden: {},
+	show: {
+		transition: {
+			staggerChildren: 0.05,
+		},
+	},
 	
-	return (
-		<div className="my-10 w-full grid gap-6 lg:grid-cols-4 justify-items-center md:grid-cols-3 sm:grid-cols-2 grid-cols-2">
-			<SkillCard svgIcon={<HtmlSVG/>} text='HTML'/>
-			<SkillCard svgIcon={<CssSVG/>} text='CSS'/>
-			<SkillCard svgIcon={<TailwindSVG/>} text='Tailwind CSS'/>
-			<SkillCard svgIcon={<JavaScriptSVG/>} text='JavaScript'/>
-			<SkillCard svgIcon={<TypeScriptSVG/>} text='TypeScript'/>
-			<SkillCard svgIcon={<ReactSVG/>} text='React'/>
-			<SkillCard svgIcon={<NextJsSVG/>} text='Next JS'/>
-			<SkillCard svgIcon={<TrpcSVG/>} text='TRPC'/>
-			<SkillCard svgIcon={<PrismaSVG/>} text='Prisma'/>
-			<SkillCard svgIcon={<SqlSVG/>} text='SQL'/>
-			<SkillCard svgIcon={<GitSVG/>} text='GIT'/>
-			<SkillCard svgIcon={<NodJsSVG/>} text='Node JS'/>
-		</div>
-	)
 }
 
+const item = {
+	hidden: { opacity: 0, y: 30 }, 
+	show: { 
+		opacity: 1,
+		y: 0,
+		transition: {
+			duration: 0.2,
+		},
+	},
+}
+
+export default function SkillsTree() {
+
+
+
+
+	return (
+		<motion.div
+			variants={container}
+			initial="hidden"
+			animate="show"
+			className="my-10 w-full grid gap-6 lg:grid-cols-4 justify-items-center md:grid-cols-3 sm:grid-cols-2 grid-cols-2"
+		>
+			<motion.div variants={item}>
+				<SkillCard svgIcon={<HtmlSVG />} text="HTML" />
+			</motion.div>
+			<motion.div variants={item}>
+				<SkillCard svgIcon={<CssSVG />} text="CSS" />
+			</motion.div>
+			<motion.div variants={item}>
+				<SkillCard svgIcon={<TailwindSVG />} text="Tailwind CSS" />
+			</motion.div>
+			<motion.div variants={item}>
+				<SkillCard svgIcon={<JavaScriptSVG />} text="JavaScript" />
+			</motion.div>
+			<motion.div variants={item}>
+				<SkillCard svgIcon={<TypeScriptSVG />} text="TypeScript" />
+			</motion.div>
+			<motion.div variants={item}>
+				<SkillCard svgIcon={<ReactSVG />} text="React" />
+			</motion.div>
+			<motion.div variants={item}>
+				<SkillCard svgIcon={<NextJsSVG />} text="Next JS" />
+			</motion.div>
+			<motion.div variants={item}>
+				<SkillCard svgIcon={<TrpcSVG />} text="TRPC" />
+			</motion.div>
+			<motion.div variants={item}>
+				<SkillCard svgIcon={<PrismaSVG />} text="Prisma" />
+			</motion.div>
+			<motion.div variants={item}>
+				<SkillCard svgIcon={<SqlSVG />} text="SQL" />
+			</motion.div>
+			<motion.div variants={item}>
+				<SkillCard svgIcon={<GitSVG />} text="GIT" />
+			</motion.div>
+			<motion.div variants={item}>
+				<SkillCard svgIcon={<NodJsSVG />} text="Node JS" />
+			</motion.div>
+		</motion.div>
+	)
+}
