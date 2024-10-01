0b356a80f1791791fb83399e1774f72f096ffe7b
Captain-Leftovers <beeondweb@gmail.com>
2024-10-01 10:09:16 +1000

    Refactor HireMe component styles for better responsiveness

--- src/app/components/HireMe.tsx
+++ src/app/components/HireMe.tsx
@@ -8,7 +8,7 @@ export default function HireMe() {
 	return (
 		<div className="xl:fixed flex items-center justify-center xl:-left-2 xl:bottom-4">
 			<MagneticWrapper>
-				<div className="relative flex items-center justify-center w-32 sm:w-48  h-auto">
+				<div className="relative flex items-center justify-center w-36 sm:w-48  h-auto">
 					<CircularText
 						className={
 							'fill-text dark:fill-text-dark animate-spin-slower'
@@ -16,7 +16,7 @@ export default function HireMe() {
 					/>
 					<Link
 						href="/contact"
-						className="absolute flex items-center justify-center w-2/4 h-2/4 font-medium transition-all duration-200  rounded-full dark:bg-primary-dark  bg-primary dark:text-text-dark text-text hover:scale-105 hover:bg-primary/90 active:scale-100 text-xs sm:text-base md:text-lg"
+						className="absolute flex items-center justify-center w-2/4 h-2/4 font-medium transition-all duration-200  rounded-full dark:bg-primary-dark  bg-primary dark:text-text-dark text-text hover:scale-105 hover:bg-primary/90 active:scale-100 text-sm sm:text-base md:text-lg"
 					>
 						Hire Me
 					</Link>
